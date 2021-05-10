e87c13159f47be7e90c234ba441f889370088bc3
VladislavBryl <54996000+VladislavBryl@users.noreply.github.com>
2021-05-10 02:10:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ process
     console.error(reason, 'Unhandled Rejection at Promise', p);
   })
   .on('uncaughtException', err => {
-    Core.log.error(err);
+    console.log(err);
   })
   .on('exit', () => {
-    Core.log.info('Shutdown\n\n\n');
+    console.log('Shutdown\n\n\n');
   });
 
 const app = new App('.', process.env.PORT);
