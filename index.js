89e8346fcedfeb62dfae1b23d5f9f868e0418eb5
VladislavBryl <54996000+VladislavBryl@users.noreply.github.com>
2021-05-10 02:04:32 +0200

    Create application back-end entry point

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Точка входа в приложение
+
+const App = require('./app');
+
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', err => {
+    Core.log.error(err);
+  })
+  .on('exit', () => {
+    Core.log.info('Shutdown\n\n\n');
+  });
+
+App.start();
